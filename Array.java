2ebf852361afcdd97e093c5a4613af0ec1a87f73
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2024-09-13 23:51:56 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class proj1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] marks= new int[5];
+		marks[0] = 100;
+		marks[1] =98;
+		marks[2] =65;
+		marks[3] =55;
+		marks[4] =33;
+		
+		System.out.println(marks[4]);
+
+  }
+
+}
