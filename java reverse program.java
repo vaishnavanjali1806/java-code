844491cf1ccaee1134597a00458f275f2177c8f7
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-13 23:07:31 +0530

    Update java reverse program.java

--- java reverse program.java
+++ java reverse program.java
@@ -1,1 +1,13 @@
 
+public class program8 {
+    public static void main(String[] args) {
+        int num = 23456;
+        int ans = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+   
