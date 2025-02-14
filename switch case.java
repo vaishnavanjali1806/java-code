db5307cf84428be16797f7b64452ddce2f20ff89
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-14 15:03:15 +0530

    Update switch case.java

--- switch case.java
+++ switch case.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class program10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String day = in.next();
+
+        // switch (day) {
+        //     case "Sunday":
+        //     case "Monday":
+        //     case "Tuesday":
+        //     case "Weansday":
+        //     case "Thusday":
+        //     System.out.println("Weekday");
+        //     case "Friday":
+        //     case "Saturday":
+        //     System.out.println("Weekday");
+
+
+        // }
+        switch (day){
+            case "Sunday", "Monday", "Tuesday", "Weansday", "Thusday" -> System.out.println("Weekday");
+            case "Friday", "Saturday" -> System.out.println("Weekday");
+        }
+    }
+    
+}
 
