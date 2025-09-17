d087847f967614ebe8296a1101b2785179e7a650
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-09-17 18:46:30 +0530

    Create code4.java

---
+++ code4.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+ class program24 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        System.out.print(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+       
+        return nums[n / 2];
+    }
+}
