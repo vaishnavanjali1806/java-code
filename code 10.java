a9e31f0abdffa8b27cb3742a2bbca657fb6cef2d
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-09-20 13:22:12 +0530

    Create code 10.java

---
+++ code 10.java
@@ -0,0 +1,38 @@
+public class program40 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1};
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, swap with just larger element
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        // Step 3: Reverse the suffix
+        int l = i + 1, r = n - 1;
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+
+        // Step 4: Print result
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
