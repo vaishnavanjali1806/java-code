1aa97a5778e9826243f70d566ebfdaa7d1be0d49
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-09-21 11:28:50 +0530

    Create code14.java

---
+++ code14.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class program45 {
+    public static void main(String[] args) {
+        int[] nums= {3,2,1,5,6,4};
+        int k = 2;
+        Arrays.sort(nums);
+        System.out.println(nums[nums.length-k]);
+    }
+    
+}
