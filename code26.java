17badb02cf0dc8df6a04bc3da75913c6ab476bd5
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-09-25 10:52:06 +0530

    Create code26.java

---
+++ code26.java
@@ -0,0 +1,13 @@
+public class program56 {
+    public static void main(String[] args) {
+        String str = "";
+        for(int i = 1; i <= 10 ; i++){
+            str = str+i;
+
+        }
+        System.out.println(str);
+    }
+}
+
+    
+
