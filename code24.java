e215704e5fd2f15a36fb5b9b0009b9cad82f677e
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-09-25 10:43:17 +0530

    Create code24.java

---
+++ code24.java
@@ -0,0 +1,11 @@
+public class program55 {
+    public static void main(String[] args) {
+        String str = "abcd";
+        for(int i = 0; i <= 3; i++){
+            for(int j = i+1; j <= 4; j++){
+                System.out.println(str.substring(i, j));
+            }
+        }
+    }
+    
+}
